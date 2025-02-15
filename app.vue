41ae8484ee246bd1facc85f239cd6c7fae766760
GeekBro1337 <kenesov_alisher@mail.ru>
2025-02-15 19:14:13 +0500

    Nuxt + tailwind installing

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <UCard>
+      <h2 class="text-xl font-bold text-blue-600">✅ Nuxt UI работает!</h2>
+      <p class="mt-2 text-gray-600">
+        Теперь ты можешь использовать UI-компоненты прямо в проекте.
+      </p>
+      <UButton color="primary" class="mt-4">Кликни меня</UButton>
+    </UCard>
+  </div>
+</template>
